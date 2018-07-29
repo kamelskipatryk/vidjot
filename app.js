607be0039a61cfc6aae3145674cea7017f670755
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-07-29 15:29:15 +0200

    add test routes

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
-const port = 5000;
+// Index Route
+app.get('/', (req, res) => {
+    res.send('index');
+});
 
+// About Route
+app.get('/about', (req, res) => {
+    app.send('About!');
+});
+
+
+const port = 5000;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-})
+});
