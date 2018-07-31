dae019b11a7014c5684a47426513ba31feb807d1
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-07-31 21:56:19 +0200

    add server side form validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -16,6 +17,10 @@ mongoose.connect('mongodb://localhost:27017/vidjot-dev', { useNewUrlParser: true
 // Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Index Route
 app.get('/', (req, res) => {
@@ -35,6 +40,27 @@ app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
 });
 
+// Process Form
+app.post('/ideas', (req, res) => {
+    let errors = [];
+
+    if(!req.body.title){
+        errors.push({text: 'Please add a title!'});
+    }
+    if(!req.body.details){
+        errors.push({text: 'Please add some details!'});
+    }
+
+    if(errors.length > 0){
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        res.send('passed');
+    }
+});
 
 // Port
 const port = 5000;
