f7b565fbff661b03b6d899f55aa7148df9bf74ad
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-08-01 20:04:58 +0200

    fetching ideas from local MongoDB

--- app.js
+++ app.js
@@ -35,6 +35,18 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+    Idea.find({})
+     .sort({date:'desc'})
+     .then(ideas => {
+         res.render('ideas/index', {
+             ideas: ideas
+         })
+     })
+});
+
+
 // Add Idea Form
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
