cb3b5d6464f772b8890ec8af9a2ad62d293cd442
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-08-05 14:26:54 +0200

    update idea data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -21,6 +22,9 @@ app.set('view engine', 'handlebars');
 //Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Method override middleware
+app.use(methodOverride('_method'));
 
 // Index Route
 app.get('/', (req, res) => {
@@ -92,6 +96,23 @@ app.post('/ideas', (req, res) => {
              res.redirect('/ideas');
          })
     }
+});
+
+// Edit Form process
+app.put('/ideas/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        //new values
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+
+        idea.save()
+         .then(idea => {
+             res.redirect('/ideas');
+         })
+    })
 });
 
 // Port
