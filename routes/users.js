f937a65106bcd97ce92d215e7e050c397b7080bf
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-08-11 17:01:46 +0200

    add register form validation

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,29 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
+// Register Form POST
+router.post('/register', (req, res) => {
+    let errors = [];
+
+    if(req.body.password != req.body.confirm_password){
+        errors.push({text:'Passwords do not match'});
+    }
+
+    if(req.body.password.length < 4){
+        errors.push({text:'Password is too short, min. 4 characters'});
+    }
+
+    if(errors.length > 0){
+        res.render('users/register',{
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            confirm_password: req.body.confirm_password
+        });
+    } else {
+        res.send('passed');
+    }
+});
+
 module.exports = router;
