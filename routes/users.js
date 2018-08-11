a3527e98dee0aae36337568e36cf26063210040b
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-08-11 19:31:19 +0200

    fixing the repeat email issue in database

--- routes/users.js
+++ routes/users.js
@@ -38,27 +38,35 @@ router.post('/register', (req, res) => {
             confirm_password: req.body.confirm_password
         });
     } else {
-        const newUser = new User ({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        })
-        
-        bcrypt.genSalt(10, (errors, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if(err) throw err;
-                newUser.password = hash;
-                newUser.save()
-                 .then(user => {
-                     req.flash('succes_msg', 'You are now registered and can log in');
-                     res.redirect('/users/login');
-                 })
-                 .catch(err => {
-                     console.log(err);
-                     return;
-                 })
-            })
-        });
+        User.findOne({email: req.body.email})
+         .then(user => {
+             if(user){
+                req.flash('error_msg', 'Email is currently using!');
+                res.redirect('/users/register');
+             } else {
+                const newUser = new User ({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+                
+                bcrypt.genSalt(10, (errors, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                         .then(user => {
+                             req.flash('succes_msg', 'You are now registered and can log in');
+                             res.redirect('/users/login');
+                         })
+                         .catch(err => {
+                             console.log(err);
+                             return;
+                         })
+                    })
+                });
+             }
+         })
     }
 });
 
