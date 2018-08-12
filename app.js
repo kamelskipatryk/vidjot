b3d08a7381fe642d268c6e2ce8d485f82a65b92d
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-08-12 11:46:32 +0200

    finishing the login functionality

--- app.js
+++ app.js
@@ -41,7 +41,11 @@ app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
-  }))
+  }));
+
+// Passport middleware - put this after the express middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(flash());
 
@@ -50,6 +54,7 @@ app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 })
 
--- config/passport.js
+++ config/passport.js
@@ -7,6 +7,33 @@ const User = mongoose.model('users');
 
 module.exports = (passport) => {
     passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
-        console.log(email);
+        // Match user
+        User.findOne({
+            email: email
+        }).then(user => {
+            if(!user){
+                return done(null, false, {message: 'No User Found'});
+            }
+
+            // Match password
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if(err) throw err;
+                if (isMatch) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, {message: 'Password incorrect'});
+                }
+            })
+        })
     }));
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+          done(err, user);
+        });
+      });
 }
