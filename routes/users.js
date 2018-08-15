0423351c9a251eec799d4e73b9750df7c2b9f024
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-08-15 22:51:57 +0200

    fixed logout 500 error

--- routes/users.js
+++ routes/users.js
@@ -81,7 +81,7 @@ router.post('/register', (req, res) => {
 });
 
 // Logout User
-router.get('/logout', (res, req) => {
+router.get('/logout', (req, res) => {
     req.logout();
     req.flash('success_msg', 'You are logged out');
     res.redirect('/users/login');
